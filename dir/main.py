5dd7819aeb1851982197dc9d13872b47e930bc2d
Elizabeth <eroberts9789@gmail.com>
2021-05-11 11:15:37 -0700

    Added dir, parsing ACLSV.gbk

---
+++ dir/main.py
@@ -0,0 +1,37 @@
+
+"""
+GOAL:   SCAN THROUGH MULTIPLE SEQUENCE FILES IN GENBANK FILE, EXTRACT AND PRINT ALL CDS RANGES
+Format for genbank files...
+
+LOCUS       13C210_ACLSV      7541 bp    DNA     linear   UNA 
+SOURCE      No definition (unknown)
+FEATURES             Location/Qualifiers
+     CDS             150..5801  
+                     /cds_type="ORF1 replicase"
+                     /note="Length: 5652"
+                     /note="Found at strand: positive"
+                     /note="Start codon: ATG"
+     CDS             5713..7095
+                     /cds_type="ORF2 movement protein"
+                     /note="Length: 1383"
+                     /note="Found at strand: positive"
+                     /note="Start codon: ATG"
+     CDS             6608..7360
+                     /cds_type="ORF3 coat protein"
+                     /note="Length: 753"
+                     /note="Found at strand: positive"
+                     /note="Start codon: ATG"
+ORIGIN
+        1 ATACTGATAC AGTGTACACT CACGTCGTGA GTAAACAGAT TGACGTAACG CCTCAATCGT....
+    
+"""
+from Bio import GenBank
+
+#open genbank file, look at each record
+with open("ACLSV.gbk") as handle:
+    for record in GenBank.parse(handle):
+        pass
+
+
+
+
