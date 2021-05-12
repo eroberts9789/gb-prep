9f590c513f83eb2d2f104c0080a751b2af149a72
Elizabeth <eroberts9789@gmail.com>
2021-05-12 11:48:26 -0700

    now printing all necessary data to features output, unsure why formatting isn't working, lots of quotation marks

--- dir/main.py
+++ dir/main.py
@@ -52,6 +52,46 @@ record = { "locus" : "asdf1234",
                                              ]
         }
 """
+
+def format_locs(location):
+    loc0 = ''
+    loc1 = ''
+    pos = 0
+    for char in location:
+        if char != '.':
+            loc0 += char
+            pos += 1
+        else:
+            break
+
+    for char in range(pos + 2, len(location)):
+        loc1 += str(location[char])
+
+    loc_tuple = (loc0, loc1)
+    return loc_tuple
+
+def format_qualifiers(qualifier):
+    quals0 = ''
+    quals1 = ''
+    pos = 0
+    for char in qualifier:
+        if char != ' ':
+            quals0 += char
+            pos += 1
+        else:
+            break
+
+    for char in range(pos +1, len(qualifier)):
+        quals1 += qualifier[char]
+    if quals1 == '':
+        quals1 = quals0
+    quals0 = str.strip(quals0)
+    quals1 = str.strip(quals1)
+    print(quals0 + quals1)
+    quals_tuple = (quals0, quals1)
+    return quals_tuple
+
+
 input_name = "ACLSV.gbk"
 output_name = "ACLSVfeatures.txt"
 
@@ -69,6 +109,8 @@ with open(input_name) as handle:
 
         for feature in record.features:
             Feature["features"].append(feature)
+            #print(feature.qualifiers[0].value)
+            format_qualifiers(feature.qualifiers[0].value)
 
         #print(Feature)
         record_list.append(Feature)
@@ -89,40 +131,39 @@ loc0	loc1	feature_key
                 
 lOOP BACK THROUGH, PRINTING AS WE GO!
 """
-with open ('output_name', 'w', newline='') as csvfile:
+
+with open(output_name, 'w', newline='') as file:
     for record in record_list:
-        pass
+        writer = csv.writer(file, delimiter='\t')
+
+        header_vals = ['>Feature', str(record["locus"])]
+        writer.writerow(header_vals)
+
+        for feature in record["features"]:
+            #print line: loc0	loc1	feature_key
+            locs = format_locs(feature.location)
+
+            loc_line_vals = [str(locs[0]), str(locs[1]), str(feature.key)]
+            writer.writerow(loc_line_vals)
+
+            #print lines: product	<first word in qualifier desc>
+            qualifiers = format_qualifiers(feature.qualifiers[0].value)
+
+            product_line_list = [['Product', qualifiers[0]], ['Product', qualifiers[1]]]
+            writer.writerows(product_line_list)
 
 
 
 
-"""
-#PROCEDURE FOR GETTING LOCATION TUPLES FROM 123..1234 FORMAT FOR LOCATIONS
-locations = []
-for cds in Features:
-    locations.append(cds.location)
-location_tuples = []
-for loc in locations:
-    loc0 = ''
-    loc1 = ''
-    pos = 0
-    for char in loc:
-        if char != '.':
-            loc0 += char
-            pos += 1
-        else:
-            break
-
-    for char in range(pos+2, len(loc)):
-        loc1 += str(loc[char])
-
-    temp_tuple = (loc0, loc1)
-    location_tuples.append(temp_tuple)
 
 
-print(location_tuples)
 
-"""
+
+
+
+
+
+
 
 
 
