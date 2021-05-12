ac8279ed7fe372ea5ce762d28b937dd80417141b
Elizabeth <eroberts9789@gmail.com>
2021-05-12 10:05:40 -0700

    now looping through all records to extract needed info and put into record dicts. Started looking at csv output formatting.

--- dir/main.py
+++ dir/main.py
@@ -2,7 +2,7 @@ from Bio import GenBank
 import csv
 
 """
-Format for genbank files...
+FORMAT FOR INPUT/GENBANK FILE WHICH WE WANT TO EXTRACT DESIRED DATA FROM
 
 LOCUS       13C210_ACLSV      7541 bp    DNA     linear   UNA 
 SOURCE      No definition (unknown)
@@ -24,30 +24,83 @@ FEATURES             Location/Qualifiers
                      /note="Start codon: ATG"
 ORIGIN
         1 ATACTGATAC AGTGTACACT CACGTCGTGA GTAAACAGAT TGACGTAACG CCTCAATCGT....
+			
 
-WORKFLOW:
--store locus for each record
--store all locations + location data for each record
--output with cvs
+HOW DATA IS STORED IN THIS FILE BEFORE OUTPUT:
+
+we loop through all records in genbank file and create a list of record dictionaries named record_list that will conatain all info we want to output
+record dictionaries will be formatted as:
+
+record = { "locus" : "asdf1234",
+           "features" : [     CDS             150..5801
+                                             /cds_type="ORF1 replicase"
+                                             /note="Length: 5652"
+                                             /note="Found at strand: positive"
+                                             /note="Start codon: ATG",
+                                             
+                              CDS             150..5801
+                                             /cds_type="ORF1 replicase"
+                                             /note="Length: 5652"
+                                             /note="Found at strand: positive"
+                                             /note="Start codon: ATG",
+                                             
+                              CDS             150..5801
+                                             /cds_type="ORF1 replicase"
+                                             /note="Length: 5652"
+                                             /note="Found at strand: positive"
+                                             /note="Start codon: ATG"
+                                             ]
+        }
 """
+input_name = "ACLSV.gbk"
+output_name = "ACLSVfeatures.txt"
 
-with open("ACLSV.gbk") as handle:
+record_list = []
 
-    Features = []
+with open(input_name) as handle:
 
     for record in GenBank.parse(handle):
-        #print(record.locus)
+        Feature = {
+            "locus": "",
+            "features": [],
+        }
+
+        Feature["locus"] = record.locus
+
         for feature in record.features:
-            Features.append(feature)
-            #print(feature.qualifiers[0].value)
-            #print("\n")
+            Feature["features"].append(feature)
+
+        #print(Feature)
+        record_list.append(Feature)
+
+"""
+NOW WE WRITE OUTPUT USING CSV IN THE FORMAT SHOWN BELOW:
+
+>Feature	<LOCUS>
+loc0	loc1	feature_key
+                product	<first word in qualifier desc>
+                product	<other words in qualidier desc>
+loc0	loc1	feature_key
+                product	<first word in qualifier desc>
+                product	<other words in qualidier desc>
+loc0	loc1	feature_key
+                product	<first word in qualifier desc>
+                product	<other words in qualidier desc>
+                
+lOOP BACK THROUGH, PRINTING AS WE GO!
+"""
+with open ('output_name', 'w', newline='') as csvfile:
+    for record in record_list:
+        pass
 
 
 
+
+"""
+#PROCEDURE FOR GETTING LOCATION TUPLES FROM 123..1234 FORMAT FOR LOCATIONS
 locations = []
 for cds in Features:
     locations.append(cds.location)
-
 location_tuples = []
 for loc in locations:
     loc0 = ''
@@ -69,7 +122,7 @@ for loc in locations:
 
 print(location_tuples)
 
-
+"""
 
 
 
