94888999dcc94eaf1a65f4af1fd7ff7d28c7e09d
Elizabeth <eroberts9789@gmail.com>
2021-05-13 12:57:03 -0700

    When input is formatted correctly this works perfectly. I now need to figure out how to get the output for all records to match the output from the first record because the input from the first record is the only one formatted correctly.

--- dir/main.py
+++ dir/main.py
@@ -1,20 +1,27 @@
+
 from Bio import GenBank
 import csv
 
 """
 FORMAT FOR INPUT/GENBANK FILE WHICH WE WANT TO EXTRACT DESIRED DATA FROM
 
-LOCUS       13C202_PNRSV_RNA2      2572 bp    DNA     linear   UNA 
+LOCUS       13C214_PNRSV_RNA3      1943 bp    RNA     linear   VRL 03-MAY-2012
 FEATURES             Location/Qualifiers
-     CDS             18..2417
+     CDS             174..1025
                      /cds_type="ORF"
-                     /product="polymerase p2"
-                     /note="Length: 2400"
+                     /product="movement protein"
+                     /note="Length: 852"
+                     /note="Found at strand: positive"
+                     /note="Start codon: ATG"
+     CDS             1100..1774
+                     /cds_type="ORF"
+                     /product="coat protein"
+                     /note="Length: 675"
                      /note="Found at strand: positive"
                      /note="Start codon: ATG"
 ORIGIN
-        1 CTCGTGGTTG AGTTACAATG AATCCTTTGA ATCAGTTGTT GACACATGGG TGTACTGCTA
-			
+        1 GTTTTTACAA TTGAAATCGT AACATCCAGC AATTGGTTGA TTTCACTTTT GCTTGACTAG
+
 
 HOW DATA IS STORED IN THIS FILE BEFORE OUTPUT:
 
@@ -38,99 +45,78 @@ def format_locs(location):
         loc1 += str(location[x])
 
     locs = [loc0, loc1]
-    #print (loc0 + " " +loc1)
+    # print (loc0 + " " +loc1)
     return locs
 
+input_name = "PNRSV_RNA3.gbk"
+output_name = "features_output.txt"
 
-input_name = "PNRSV_RNA1.gbk"
-output_name = "ACLSVfeatures.txt"
-
-record_list = []
+"""
+PARSE GENBANK FILE AND STORE ALL REQUIRED INFO IN RECORD DICTIONARIES IN RECORD_LIST LIST
+"""
 
 with open(input_name) as handle:
+    record_list = []
+    for gbk_record in GenBank.parse(handle):
 
-    first_record = True
-    first_format = {
-        'feature_keys' : [],
-        'feature_type' : '',
-        'product' : ''
-    }
-
-    for record in GenBank.parse(handle):
-
-        record_info = {
+        record = {
             "locus": "",
             "features": []
         }
 
-        record_info["locus"] = record.locus
+        record["locus"] = gbk_record.locus
 
-        for feature in record.features:
+        for feature in gbk_record.features:
+            record["features"].append(feature)
 
-            #only do this for first record, this is the record that is best formatted and the rest of the feature qualifiers should match this format in the output
-            if first_record == True:
-                first_format['feature_keys'].append(feature.key)
-                for qualifier in feature.qualifiers:
-                    if "type" in qualifier.key:
-                        first_format['feature_type'] = qualifier.value
-                        #print(first_format['feature_type'])
-                    if "product" in qualifier.key:
-                        first_format['product'] = qualifier.value
-                        #print(first_format['product'])
-            first_record = False
-
-            #only save location output for rest of records because features output will match first record
-            record_info["features"].append(feature)
-
-        record_list.append(record_info)
-
-
+        record_list.append(record)
 
 """
-NOW WE WRITE OUTPUT USING CSV IN THE FORMAT SHOWN BELOW:
-
->Feature	<LOCUS>
-loc0	loc1	feature_key
-                product	<first word in qualifier desc>
-                product	<other words in qualifier desc>
-loc0	loc1	feature_key
-                product	<first word in qualifier desc>
-                product	<other words in qualifier desc>
-loc0	loc1	feature_key
-                product	<first word in qualifier desc>
-                product	<other words in qualifier desc>
-                
-lOOP BACK THROUGH, PRINTING AS WE GO!
+NOW WRITE EVERYTHING TO FEATURES FILE
 """
-
 
 with open(output_name, 'w', newline='') as output_file:
     writer = csv.writer(output_file, delimiter='\t', escapechar=' ', quoting=csv.QUOTE_NONE)
 
     for record in record_list:
-
-        header_vals = ['>Feature', record["locus"]]
-        writer.writerow(header_vals)
+        """
+        write header line in the format:
+        >Feature	<LOCUS>
+        """
+        header_format = ['>Feature', record["locus"]]
+        writer.writerow(header_format)
 
         for feature in record["features"]:
-            if feature.key in first_format['feature_keys']:
-                locs = format_locs(feature.location)
+            """
+            write location line in the format:
+            <loc0>	<loc1>	<feature.key>
+            """
+            locs = format_locs(feature.location)
+            loc_line_format = [locs[0], locs[1], str(feature.key)]
+            writer.writerow(loc_line_format)
 
-                loc_line_vals = [locs[0], locs[1], str(feature.key)]
-                writer.writerow(loc_line_vals)
-
-                feature_type = str.strip(first_format['feature_type'], '"')
-                product = str.strip(first_format['product'], '"')
-
-                if len(feature_type) > 0:
-                    type_line_format = ['\t', '\t', 'Product', feature_type]
+            for qualifier in feature.qualifiers:
+                """
+                looks for qualifier.key named "feature_type". If found, print line in the format:
+                 		 		Product	<feature_type>
+                """
+                if 'type' in qualifier.key:
+                    type = str.strip(qualifier.value, '"')
+                    type_line_format = ['\t', '\t', 'Product', type]
                     writer.writerow(type_line_format)
-                if len(product) > 0:
+                """
+                looks for qualifier.key named "product". If found, print line in the format:
+                 		 		Product	<product>
+                """
+                if 'product' in qualifier.key:
+                    product = str.strip(qualifier.value, '"')
                     product_line_format = ['\t', '\t', 'Product', product]
                     writer.writerow(product_line_format)
-
+                """
+                IF OTHER QUALIFIERS ARE NEEDED THEN FORMAT LIKE ABOVE
+                """
+        #newline
         writer.writerow('\t')
-
 
 
 
