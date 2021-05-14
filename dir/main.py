fbe3023ea147de4c48ac9f45416eafd3586b4dae
Elizabeth <eroberts9789@gmail.com>
2021-05-14 11:01:56 -0700

    Validate output, then push to git

--- dir/main.py
+++ dir/main.py
@@ -2,40 +2,19 @@
 from Bio import GenBank
 import csv
 
-"""
-FORMAT FOR INPUT/GENBANK FILE WHICH WE WANT TO EXTRACT DESIRED DATA FROM
 
-LOCUS       13C214_PNRSV_RNA3      1943 bp    RNA     linear   VRL 03-MAY-2012
-FEATURES             Location/Qualifiers
-     CDS             174..1025
-                     /cds_type="ORF"
-                     /product="movement protein"
-                     /note="Length: 852"
-                     /note="Found at strand: positive"
-                     /note="Start codon: ATG"
-     CDS             1100..1774
-                     /cds_type="ORF"
-                     /product="coat protein"
-                     /note="Length: 675"
-                     /note="Found at strand: positive"
-                     /note="Start codon: ATG"
-ORIGIN
-        1 GTTTTTACAA TTGAAATCGT AACATCCAGC AATTGGTTGA TTTCACTTTT GCTTGACTAG
-
-
-HOW DATA IS STORED IN THIS FILE BEFORE OUTPUT:
-
-we loop through all records in genbank file and create a list of record dictionaries named record_list that will conatain all info we want to output
-record dictionaries will be formatted as:
-
-"""
 global input_name
-input_name = "PNRSV_RNA3.gbk"
+input_name = "ACLSV.gbk"
 global output_name
 output_name = "features_output.txt"
-formatted_right = False
+
 
 def format_locs(location):
+    """
+    Takes string describing loaction and returns nicely formatted tuple
+    :param location: location string in the format 123..1234
+    :return: location tuple (123, 1234)
+    """
     loc0 = ''
     loc1 = ''
     pos = 0
@@ -53,7 +32,15 @@ def format_locs(location):
     # print (loc0 + " " +loc1)
     return locs
 
-def format_record_list(first_record, record_list):
+def format_record_list(first_record : list, record_list : list):
+    """
+    Takes full list of input records and removes any repeated features and any feature keys that don't match the feature keys in the first record. Prints list of incorrectly formatted features if contains extra features after these steps
+
+    :param first_record: record formatted correctly, compare others to this format
+    :param record_list: list of all records from gbk file
+    :return: list of records only containing records with the correct number of features that match desired format
+    """
+
     first_record_feature_keys = []
     for feature in first_record['features']:
         first_record_feature_keys.append(feature.key)
@@ -61,24 +48,22 @@ def format_record_list(first_record, record_list):
     must_fix_format = []
     result = []
     for record in record_list:
-        """
-        REMOVE ANY REPEATED FEATURES BY TURNING INTO DICT AND THEN BACK TO LIST
-        """
+        #REMOVE ANY REPEATED FEATURES BY TURNING INTO DICT AND THEN BACK TO LIST
         record['features'] = list(dict.fromkeys(record['features']))
 
         for feature in record['features']:
-            """
-            IF THERE IS A FEATURE KEY THAT DOESN'T BELONG IN THE OUTPUT, REMOVE IT FROM THE RECORD IN THE RECORD_LIST
-            """
+            #IF THERE IS A FEATURE KEY THAT DOESN'T MATCH THE FEATURE KEYS IN THE FIRST FEATURE, REMOVE THAT FEATURE FROM RECORD
             if feature.key not in first_record_feature_keys:
                 record['features'].remove(feature)
 
+        #CREATE LIST OF POORLY FORMATTED RECORDS THAT NEED TO BE FIXED
         if len(record['features']) != len(first_record_feature_keys):
             must_fix_format.append(record)
-
+        #CREATE LIST OF CORRECTLY FORMATTED RECORDS
         if record not in must_fix_format:
             result.append(record)
 
+    #IF LIST CONTAINS ANY POORLY FORMATTED RECORDS, PRINT THEM OUT
     global formatted_right
     if len(must_fix_format) > 0:
         formatted_right = False
@@ -91,39 +76,30 @@ def format_record_list(first_record, record_list):
 
 
 
-"""
-PARSE GENBANK FILE AND STORE ALL REQUIRED INFO IN RECORD DICTIONARIES IN RECORD_LIST LIST
-"""
 
+#PARSE GENBANK FILE AND STORE ALL REQUIRED INFO IN RECORD DICTIONARIES IN RECORD_LIST LIST
 with open(input_name) as handle:
+
     record_list = []
     for gbk_record in GenBank.parse(handle):
-
         record = {
             "locus": "",
             "features": []
         }
-
         record["locus"] = gbk_record.locus
-
         for feature in gbk_record.features:
             record["features"].append(feature)
-
         record_list.append(record)
 
-"""
-update records in records list so that they all match the format of the first record
-"""
 first_record = record_list[0]
-all_recs = format_record_list(first_record, record_list)
+record_list = format_record_list(first_record, record_list)
 
 
-"""
-NOW WRITE EVERYTHING TO FEATURES FILE
-"""
-
+#NOW WRITE EVERYTHING TO FEAUTURES FILE
 with open(output_name, 'w', newline='') as output_file:
     writer = csv.writer(output_file, delimiter='\t', escapechar=' ', quoting=csv.QUOTE_NONE)
+
+    #THIS ONLY APPLIES IF RECORD WERE ALREADY ALL FORMATTED WITH CORRECT FEATURES
     if formatted_right == True:
         for record in record_list:
             """
@@ -159,10 +135,7 @@ with open(output_name, 'w', newline='') as output_file:
                         product = str.strip(qualifier.value, '"')
                         product_line_format = ['\t', '\t', 'Product', product]
                         writer.writerow(product_line_format)
-                    """
-                    IF OTHER QUALIFIERS ARE NEEDED THEN FORMAT LIKE ABOVE
-                    """
-            #newline
+
             writer.writerow('\t')
 
     else:
@@ -173,6 +146,7 @@ with open(output_name, 'w', newline='') as output_file:
             """
             header_format = ['>Feature', record["locus"]]
             writer.writerow(header_format)
+
             for feature, first_feature in zip(record["features"], first_record['features']):
                 """
                 write location line in the format:
@@ -182,6 +156,7 @@ with open(output_name, 'w', newline='') as output_file:
                 loc_line_format = [locs[0], locs[1], str(feature.key)]
                 writer.writerow(loc_line_format)
 
+                #ITERATES THROUGH AND PRINTS QUALIFIERS IN FIRST FILE SO ALL OUTPUT MATCHES
                 for qualifier in first_feature.qualifiers:
                     """
                     looks for qualifier.key named "feature_type". If found, print line in the format:
@@ -199,9 +174,9 @@ with open(output_name, 'w', newline='') as output_file:
                         product = str.strip(qualifier.value, '"')
                         product_line_format = ['\t', '\t', 'Product', product]
                         writer.writerow(product_line_format)
-                    """
-                    IF OTHER QUALIFIERS ARE NEEDED THEN FORMAT LIKE ABOVE
-                    """
+
+            writer.writerow('\t')
+
 
 
 
